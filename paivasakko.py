a07423a31798ead9a5982b6df84b1de18d91e0ee
Aija Valleala <aija.valleala@gmail.com>
2021-05-13 08:42:39 +0300

    korjauksia

--- paivasakko.py
+++ paivasakko.py
@@ -1,25 +1,30 @@
+import math
+
 def laske_yhden_paivasakon_maara(netto_kuukausitulo, elatettavia):
     peruskulutusvahennus = 255
-    paivasakon_vahin_maara = 6
-    elatusvahennus = 3
+    paivasakon_minimi_maara = 6
+    elatusvahennus = 3 * elatettavia
 
-    paivasakon_perusteena_oleva_tulo = netto_kuukausitulo - peruskulutusvahennus - elatusvahennus * elatettavia
+    paivasakon_perusteena_oleva_tulo = netto_kuukausitulo - peruskulutusvahennus
 
-    yhden_paivasakon_maara = max(paivasakon_perusteena_oleva_tulo / 60, paivasakon_vahin_maara)
-    
-    return yhden_paivasakon_maara
+    yhden_paivasakon_maara = (paivasakon_perusteena_oleva_tulo / 60) - elatusvahennus
+
+    yhden_paivasakon_maara_pyoristetty_alas = math.floor(yhden_paivasakon_maara)
+
+    if yhden_paivasakon_maara_pyoristetty_alas < paivasakon_minimi_maara:
+        return paivasakon_minimi_maara
+    else:
+        return yhden_paivasakon_maara_pyoristetty_alas
     
 
-def laske_sakon_kokonaismaara(yhden_paivasakon_maara, kuinka_monta, ylinopeussakko_mopoilija, ylinopeussakko_muu):
-    mopoilijan_ylinopeussakon_minimimaara = 100
-    muun_ylinopeussakon_minimimaara = 200
-    
+def laske_sakon_kokonaismaara(yhden_paivasakon_maara, kuinka_monta, ylinopeussakko):
     sakon_kokonaismaara = yhden_paivasakon_maara * kuinka_monta
 
-    if ylinopeussakko_mopoilija:
-        return max(sakon_kokonaismaara, mopoilijan_ylinopeussakon_minimimaara)
-    elif ylinopeussakko_muu:
-        return (max(sakon_kokonaismaara, muun_ylinopeussakon_minimimaara))
+    ylinopeussakko_minimimaarat = {"mopo": 100, "muu_moottoriajoneuvo": 200, "vesikulkuneuvo": 120}
+
+    if ylinopeussakko in ylinopeussakko_minimimaarat.keys():
+        if sakon_kokonaismaara < ylinopeussakko_minimimaarat[ylinopeussakko]:
+            return ylinopeussakko_minimimaarat[ylinopeussakko]
 
     return sakon_kokonaismaara
 
@@ -28,12 +33,11 @@ def laske_sakon_kokonaismaara(yhden_paivasakon_maara, kuinka_monta, ylinopeussak
 netto_kuukausitulo = 3000
 elatettavia = 2
 
-kuinka_monta_paivasakkoa = 20
-ylinopeussakko_mopoilija = False
-ylinopeussakko_muu = True
-
 yhden_paivasakon_maara = laske_yhden_paivasakon_maara(netto_kuukausitulo, elatettavia)
 print(yhden_paivasakon_maara)
 
-sakon_kokonaismaara = laske_sakon_kokonaismaara(yhden_paivasakon_maara, kuinka_monta_paivasakkoa, ylinopeussakko_mopoilija, ylinopeussakko_muu)
+
+kuinka_monta_paivasakkoa = 20
+
+sakon_kokonaismaara = laske_sakon_kokonaismaara(yhden_paivasakon_maara, kuinka_monta_paivasakkoa, "mopo")
 print(sakon_kokonaismaara)
